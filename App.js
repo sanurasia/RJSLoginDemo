7ca5b78e4564e9cd8be2a4929b285b148e33417b
sanjay <sanjay.csjm@gmail.com>
2019-10-15 12:18:49 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,119 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import  React, { Component,useState, useEffect} from 'react';
+import {Button, View, Text,Animated} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import{ createStackNavigator } from 'react-navigation-stack';
+import SignUp from './Components/SignUp';
+import LoginForm from './Components/LoginForm';
+
+//import { Animated} from 'react-native';
+
+const FadeInView = (props) => {
+  const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
+
+  React.useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 10000,
+      }
+    ).start();
+  }, [])
+
+  return (
+    <Animated.View                 // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,         // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
+
+
+
+class HomeScreen extends React.Component {
+  render(){
+    return (
+      <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
+      
+       <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
+      </FadeInView>
+       <Button
+       title = "SignUp"
+       //onPress={() => this.props.navigation.navigate('Details', { data: { title: 'Home Screen'}
+       onPress={() => this.props.navigation.navigate('SignUp', { data: { title: 'SignUp Screen'}
+       })}
+       />
+
+      <Button
+       title = "Sign In"
+       //onPress={() => this.props.navigation.navigate('Details', { data: { title: 'Home Screen'}
+       onPress={() => this.props.navigation.navigate('Login', { data: { title: 'Login Screen'}
+       })}
+       />
+       
+       </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+   render(){
+    var { title } = this.props.navigation.state.params.data
+     return (
+       <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+         <Text> Coming from {title}</Text>
+         <Button
+         title = "Go to Details   yo to  again.. "
+         onPress= {() => this.props.navigation.push('Details')}
+         />
+         <Button
+         title = "Go To Home"
+         onPress = {() => this.props.navigation.navigate('Home')}
+         />
+         <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+       </View>
+     );
+   }
+}
+
+
+class A extends React.Component{
+  render(){
+    var { title } = this.props.navigation.state.params.data
+     return (
+       <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+         <Text> Coming from {title}</Text>
+        
+       </View>
+     );
+   }
+}
+
+
+const AppNavigator = createStackNavigator( {
+  Home: HomeScreen,
+  //Details: DetailsScreen,
+  SignUp : SignUp,
+  Login : LoginForm,
+},
+{
+  initialRouteName: 'Home',
+}
+);
+
+export default createAppContainer(AppNavigator);
---
+++ Components/LoginForm.js
@@ -0,0 +1,99 @@
+/*import React, { Component } from 'react';
+import { Container, Header, Content, Form, Item, Input, Label } from 'react-native';
+export default class LoginForm extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}*/
+
+
+import React from 'react'
+import {
+  View,
+  Button,
+  TextInput,
+  StyleSheet
+} from 'react-native'
+
+export default class LoginForm extends React.Component {
+  state = {
+    username: '', password: ''
+  }
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val })
+  }
+  signIn = async () => {
+    const { username, password} = this.state
+    try {
+      // here place your signup logic
+      console.log('user successfully signed in!: ', success)
+    } catch (err) {
+      console.log('error sign in: ', err)
+    }
+  }
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Username'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('username', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Password'
+          secureTextEntry={true}
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('password', val)}
+        />
+        
+        <Button
+          title='SignIn'
+          onPress={this.signIn}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 55,
+    backgroundColor: '#42A5F5',
+    margin: 10,
+    padding: 8,
+    color: 'white',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+
+
